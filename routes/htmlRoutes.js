a9971e936f4b793387b373e343a6fb7f46f44299
Dj Meechlouch <Meechlouch@gmail.com>
2021-02-19 22:03:06 -0600

    minor update

--- routes/htmlRoutes.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-const router = require("express").Router();
-
-module.exports = function () {
-  router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-  });
-};
